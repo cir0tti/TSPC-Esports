c6f48920c2c69fb94562c75bd7ef4035ab4696b7
cir0tti <cirottidev@gmail.com>
2026-01-28 14:13:29 -0300

    Cambio Botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 import Home from "./pages/Home";
 import Ruleta from "./pages/Ruleta";
 import Livestreams from "./pages/Livestreams";
@@ -9,12 +11,16 @@ import LoginSuccess from "./pages/LoginSuccess";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/ruleta" element={<Ruleta />} />
-      <Route path="/livestreams" element={<Livestreams />} />
-      <Route path="/pricing" element={<Pricing />} />
-      <Route path="/login-success" element={<LoginSuccess />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ruleta" element={<Ruleta />} />
+        <Route path="/livestreams" element={<Livestreams />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login-success" element={<LoginSuccess />} />
+      </Routes>
+    </>
   );
 }
